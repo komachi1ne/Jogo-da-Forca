601172c2def107172d4b8a9eb4c493bf3ca29d2b
komachi1ne <isabellevasconcelos862@gmail.com>
2024-10-10 12:25:49 -0300

    Iniciado jogo da forca

---
+++ main.js
@@ -0,0 +1,5 @@
+const palavras = ["bola", "casa", "boneca", "escola", "caderno", "caneta", "Chico"];
+let tentativasUsadas = 0;
+let letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let palavraSecreta, palavraOculta;
+let jogando;
